f4d926e8c037335bfc3630e3a4ab9ffd53031f5b
Soul-Remix <soulremix-webdev@pm.me>
2021-09-09 13:35:43 +0300

    Fix typos & use .env

--- app.js
+++ app.js
@@ -3,18 +3,21 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-// Set up mongoose connection
 const mongoose = require('mongoose');
-const MongoDb =
-  'mongodb+srv://soulRemix:bATtl9812@cluster0.vwxde.mongodb.net/express-library?retryWrites=true&w=majority';
-mongoose.connect(MongoDb, { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+require('dotenv').config();
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
+
+const Author = require('./models/author');
+
+// Set up mongoose connection
+const MongoDb = process.env.MONGO_URL;
+mongoose.connect(MongoDb, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -28,6 +31,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.get('/author', (req, res) => {
+  const author = new Author({
+    first_name: 'fghfgh',
+    family_name: 'fghgfhfg',
+    date_of_birth: '1970',
+    date_of_death: '2000',
+  });
+  author
+    .save()
+    .then(() => console.log('request sent'))
+    .catch((err) => console.log(err));
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
