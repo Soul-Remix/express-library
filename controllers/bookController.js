deea8d441a3ea46f69fdab5a7206fb5bcfed0ff7
Soul-Remix <soulremix-webdev@pm.me>
2021-09-11 15:35:15 +0300

    Create books page

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,11 +1,40 @@
 const Book = require('../models/book');
+const Author = require('../models/author');
+const Genre = require('../models/genre');
+const BookInstance = require('../models/bookinstance');
+const { NotExtended } = require('http-errors');
 
-const index = function (req, res) {
-  res.send('Not Implemented yet');
+const index = async function (req, res, next) {
+  try {
+    const book_count = await Book.countDocuments({});
+    const book_instance_count = await BookInstance.countDocuments({});
+    const book_instance_available_count = await BookInstance.countDocuments({
+      status: 'available',
+    });
+    const author_count = await Author.countDocuments({});
+    const genre_count = await Genre.countDocuments({});
+    const data = {
+      book_count,
+      book_instance_count,
+      book_instance_available_count,
+      author_count,
+      genre_count,
+    };
+    res.render('index', { title: 'Library Home', data: data });
+  } catch (err) {
+    return next(err);
+  }
 };
 
-const book_list = function (req, res) {
-  res.send('Not Implemented yet');
+const book_list = async function (req, res, next) {
+  try {
+    const data = await Book.find()
+      .populate('author')
+      .sort([['title', 'ascending']]);
+    res.render('book-list', { title: 'Book List', data: data });
+  } catch (err) {
+    return next(err);
+  }
 };
 
 const book_detail = function (req, res) {
